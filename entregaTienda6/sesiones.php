e6ba5daa90fe8540f4c34ead5b1144ccd82430ad
MARÍA CHAPARRO CABALLERO <mchaparrocaballero@gmail.com>
2025-12-11 09:47:14 +0100

    Delete entregaTienda6 directory

--- entregaTienda6/sesiones.php
+++
@@ -1,8 +0,0 @@
-<?php
-function comprobar_sesion(){
-	session_start();
-	if(!isset($_SESSION['usuario'])){	
-		header("Location: index.php?redirigido=true");//redirigido lo usamos en index para que nos muestre o no los mensajes de error//
-	}		
-}
-
